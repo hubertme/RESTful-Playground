68dd22df8f4228e37a440c859b38ce4cc1c0c701
Hubert Wang <hubert.tatra@gmail.com>
2019-01-15 14:28:37 +0700

    Change model view on admin dashboard

--- employees/models.py
+++ employees/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    employee_id = models.PositiveIntegerField()
+    employee_id = models.AutoField(primary_key=True)
     firstName = models.CharField(max_length=255)
     lastName = models.CharField(max_length=255)
-    date_created = models.DateTimeField(auto_now=False, auto_now_add=False)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.firstName
+        return 'ID: {}, Name: {} {}, Date created: {}'.format(self.employee_id, self.firstName, self.lastName, self.date_created)
