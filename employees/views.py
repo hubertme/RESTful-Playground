db0d53ea9f321f7633ce014ad9fb9f3e685c3fb6
Hubert Wang <hubert.tatra@gmail.com>
2019-01-15 14:45:14 +0700

    Implement GET method

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import Employee as employee
+from . serializers import serializer
+
 # Create your views here.
+class employeeView(APIView):
+
+    def get(self, request):
+        employees = employee.objects.all()
+        viewSerializer = serializer(employees, many=True)
+        return Response(viewSerializer.data)
+
+    def post(self, request):
+        pass
