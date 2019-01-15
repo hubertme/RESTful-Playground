8eafb9e566d55be04d8faca43a0da1e7ec87dd78
Hubert Wang <hubert.tatra@gmail.com>
2019-01-15 16:02:58 +0700

    Change model naming

--- employees/models.py
+++ employees/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Employee(models.Model):
     employee_id = models.AutoField(primary_key=True)
-    firstName = models.CharField(max_length=255)
-    lastName = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return 'ID: {}, Name: {} {}, Date created: {}'.format(self.employee_id, self.firstName, self.lastName, self.date_created)
+        return 'ID: {}, Name: {} {}, Date created: {}'.format(self.employee_id, self.first_name, self.last_name, self.date_created)
--- employees/serializers.py
+++ employees/serializers.py
@@ -5,6 +5,6 @@ from . models import Employee as employee
 class serializer(serializers.ModelSerializer):
     class Meta:
         model = employee
-        fields = ('employee_id', 'firstName', 'lastName')
+        fields = ('employee_id', 'first_name', 'last_name', 'date_created')
 
         # fields = '__all__' # To return all fields
