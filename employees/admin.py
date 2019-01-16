a647f24bd81bb0f6c61889016beb41388f0fced1
Hubert Wang <hubert.tatra@gmail.com>
2019-01-16 11:25:07 +0700

    Add admin page for News

--- employees/admin.py
+++ employees/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Employee as employee
+from . models import Employee as employee, News as news
 
 # Register your models here.
 admin.site.register(employee)
+admin.site.register(news)
